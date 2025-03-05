42f014ca3b2ad31515d722bb0542d72176c14428
LorenzoLatinnePXL <11701160@student.pxl.be>
2025-03-05 10:33:29 +0100

    Exercise started

---
+++ WpfEmailBestand/WpfEmailBestand/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfEmailBestand
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string _fileName = "Email.txt";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void readFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            string path = Environment.CurrentDirectory;
+        }
+
+        private void readDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void readDictionaryButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void writeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
